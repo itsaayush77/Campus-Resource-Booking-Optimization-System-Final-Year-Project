27f1aa73f52f2b8f3ee3fbed17fcf95558b9ef4c
Aayush Koirala <koiralaaayush147@gmail.com>
2026-01-02 18:41:13 +0545

    feat: add dynamic routing for resource categories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
+import ResourcesList from './pages/resources/ResourcesList'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/resources/:type" element={<ResourcesList />} />
         </Routes>
       </main>
       <Footer />
